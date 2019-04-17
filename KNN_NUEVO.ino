8f6f19c7030af7672fb6a984ff452f4f0e234357
GustavoPillajo <49662510+GustavoPillajo@users.noreply.github.com>
2019-04-16 20:21:01 -0500

    Add files via upload

---
+++ KNN_NUEVO.ino
@@ -0,0 +1,76 @@
+#include "datos.h" // matriz de entrenamiento
+
+int col;// moverse en cumnas en la matriz
+int fila=0; // moverseen filas en la matriz 
+float promedio=0; //determinar las distancias entre los puntos 
+float distancia; //raiz entre los puntos
+float dist_menores [2][3]; //ordenar distancias menores 
+int i=0;
+int j=0;
+int x=0;
+int k=3;
+float respuesta;
+float mayor=0;
+float aux, aux1; // auxiliares para ordenaiento de vectores 
+float num_etiquetas[2][5]; //
+float datos_entrada [5]={5.2,3.5,1.5,1.2,1}; //datos de prueba para el sistemas
+
+void setup() {
+Serial.begin(9600);
+//llenar matriz dis_menores
+for(;i<k;i++){
+      dist_menores [0][i]=4000+i;
+      dist_menores [1][i]=0;
+      }
+       i=0;
+       for(;i<k;i++){
+        num_etiquetas[0][1]=i+1;
+         num_etiquetas[1][i]=0;
+        }
+        i=0; 
+}
+void loop() {
+  for(;fila<90;fila++){
+    for(col=0;col<4;col++){
+      promedio=promedio+pow(datos_entrada[col]-matriz[fila][col],2);
+    distancia= sqrt(promedio);
+    promedio=0;
+      if (distancia<dist_menores[0][k-1]){
+          dist_menores[0][k-1]=distancia;
+          dist_menores[1][k-1]=matriz[fila][4];
+        }
+        //rdenamiento del vector 
+        for(i=0;i<k;i++){
+          for(j-i+1;j<k;j++){
+            if(num_etiquetas [0][i]>dist_menores[0][j]){
+              aux =dist_menores[0][i];
+              dist_menores [0][i]=dist_menores[0][j];
+              dist_menores [0][j]=aux;
+              // etiquetas 
+              aux1 =dist_menores[1][i];
+              dist_menores [1][i]=dist_menores[1][j];
+              dist_menores [1][j]=aux1;
+              }
+              }
+          }
+        }
+  }
+//   determinar el numero de cada etiqueta 
+  for(i=0;i<k;i++){
+    for(j=0;j<k;j++){
+      if(num_etiquetas[0][i]=dist_menores[1][j]){
+        num_etiquetas[1][i]=1+num_etiquetas[1][i];
+        }
+      }
+    }
+    }
+
+
+          
+       
+
+       
+  
+
+
+
